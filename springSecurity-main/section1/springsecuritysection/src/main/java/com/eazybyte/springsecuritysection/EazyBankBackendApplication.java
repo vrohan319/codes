afd74a45d763c6d3484e82b7a2c2a5ee756d54f7
vrohan319 <64648247+vrohan319@users.noreply.github.com>
2025-04-25 15:07:57 +0530

    Add files via upload

---
+++ springSecurity-main/section1/springsecuritysection/src/main/java/com/eazybyte/springsecuritysection/EazyBankBackendApplication.java
@@ -0,0 +1,14 @@
+package com.eazybyte.springsecuritysection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplication {
+
+	public static void
+	main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
---
+++ springSecurity-main/section1/springsecuritysection/src/main/java/com/eazybyte/springsecuritysection/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.eazybyte.springsecuritysection.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "welcome to spring app with security";
+    }
+
+
+}
